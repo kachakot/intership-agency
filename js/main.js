f33a7c9f8c9b9a9aa98f04c373ee8157cb492282
Katarzyna K <k.kotynska91@gmail.com>
2016-05-10 10:50:56 +0200

    RWD.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,85 @@
-$(document).ready(function(){
-	$('.header-box').slick({
-	  dots: false,
-	  arrows: false,
-	  infinite: true,
-	  speed: 500,
-	  fade: true,
-	  cssEase: 'linear',
-	  autoplay: true,
-	  autoplaySpeed: 3000
-	});
+$(document).ready(function () {
 
-	$('.hamburger').on('click', function(e) {
+    $('.map-wrap').easyGoogleMaps({
+        lat: 1.276816,
+        lng: 103.848346,
+        zoom: 13,
+        randomId: true,
+        markerIcon: '',
+        tooltip: '<p>You`ve clicked on marker!</p>',
+        styles: [{
+            "featureType": "water",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "color": "#acbcc9"
+            }]
+        }, {
+            "featureType": "landscape",
+            "stylers": [{
+                "color": "#f2e5d4"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#c5c6c6"
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#e4d7c6"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#fbfaf7"
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#c5dac6"
+            }]
+        }, {
+            "featureType": "administrative",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "lightness": 33
+            }]
+        }, {
+            "featureType": "road"
+        }, {
+            "featureType": "poi.park",
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "lightness": 20
+            }]
+        }, {}, {
+            "featureType": "road",
+            "stylers": [{
+                "lightness": 20
+            }]
+        }]
+    });
+    $('.header-box').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+
+    $('.hamburger').on('click', function (e) {
         $('.nav').toggleClass('active');
         e.stopPropagation();
-    })
-
-  $('.map-wrap').easyGoogleMaps({
-    lat: 1.276816,
-    lng: 103.848346,
-    zoom: 13,
-    randomId: true,
-    markerIcon: '',
-    tooltip: '<p>You`ve clicked on marker!</p>',
-    styles: [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},{"featureType":"landscape","stylers":[{"color":"#f2e5d4"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}]
-  });
+    });
 });
